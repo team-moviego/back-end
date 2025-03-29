2eef5bffdbc769956681f8e1db22411b7e3f15cb
jinhwanB <paewhal20@gmail.com>
2025-03-29 20:04:21 +0900

    update: 조회 및 삭제 sql문 자동화 및 삭제날짜 필드 추가
    엔티티 삭제 시에 sql 명령어와 조회시에 명령어에 자동적으로 추가되도록 하는 어노테이션 추가

--- src/main/java/com/hwansol/moviego/member/model/Member.java
+++ src/main/java/com/hwansol/moviego/member/model/Member.java
@@ -1,30 +1,50 @@
 package com.hwansol.moviego.member.model;
 
 import com.hwansol.moviego.config.BaseTImeEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder(toBuilder = true)
+@SQLDelete(sql = "UPDATE member SET del_date = now() WHERE id = ?")
+@SQLRestriction("del_date IS NULL")
 public class Member extends BaseTImeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String userId; // 회원아이디
+
+    @Column(nullable = false)
     private String userPw; // 회원 비밀번호
+
+    @Column(nullable = false)
     private String userEmail; // 회원 이메일
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private Role role; // 권한
+
+    @Column
+    private LocalDateTime delDate; // 삭제날짜
 
 
 }
