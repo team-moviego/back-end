ae1fed467f64c5bb7eef2410f34824899ffea045
jinhwanB <paewhal20@gmail.com>
2025-03-29 20:11:15 +0900

    feat: 시큐리티에서 사용할 회원 디테틸 서비스 생성

---
+++ src/main/java/com/hwansol/moviego/member/service/MemberDetailsService.java
@@ -0,0 +1,27 @@
+package com.hwansol.moviego.member.service;
+
+import com.hwansol.moviego.member.exception.MemberErrorCode;
+import com.hwansol.moviego.member.exception.MemberException;
+import com.hwansol.moviego.member.model.Member;
+import com.hwansol.moviego.member.model.MemberDetails;
+import com.hwansol.moviego.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUserId(memberId)
+            .orElseThrow(() -> new MemberException(MemberErrorCode.NOT_FOUND_MEMBER));
+
+        return new MemberDetails(member);
+    }
+}
