1fbb477903f96425e05968cab40267476866d74f
jinhwanB <paewhal20@gmail.com>
2025-03-29 20:10:26 +0900

    feat: 회원 관련 에러 처리를 위한 커스텀 클래스 생성

---
+++ src/main/java/com/hwansol/moviego/member/exception/MemberException.java
@@ -0,0 +1,14 @@
+package com.hwansol.moviego.member.exception;
+
+import lombok.Getter;
+
+@Getter
+public class MemberException extends RuntimeException {
+
+    private final MemberErrorCode memberErrorCode;
+
+    public MemberException(MemberErrorCode memberErrorCode) {
+        super(memberErrorCode.getMessage());
+        this.memberErrorCode = memberErrorCode;
+    }
+}
