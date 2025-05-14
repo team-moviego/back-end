e0b20a93f8fb51c73052a70156c09aac0e4390d0
jinhwanB <paewhal20@gmail.com>
2025-05-14 16:45:38 +0900

    feat: 회원 비밀번호 변경을 위한 DTO 생성

---
+++ src/main/java/com/hwansol/moviego/member/dto/MemberModifyPwDto.java
@@ -0,0 +1,40 @@
+package com.hwansol.moviego.member.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// 회원 비밀번호 변경을 위한 DTO
+public class MemberModifyPwDto {
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @Builder(toBuilder = true)
+    public static class Request {
+
+        @NotBlank(message = "기존 비밀번호를 입력해주세요.")
+        private String originPw;
+
+        @NotBlank(message = "변경할 비밀번호를 입력해주세요.")
+        @Pattern(regexp = "^[^가-힣ㄱ-ㅎㅏ-ㅣ]+$", message = "비밀번호는 영문 또는 특수문자 또는 숫자만 작성 가능합니다.")
+        @Size(min = 8, max = 20, message = "비밀번호는 8~20자 사이로 작성하여야합니다.")
+        private String newPw;
+
+        @NotBlank(message = "변경할 비밀번호를 다시 입력해주세요.")
+        private String confirmPw;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class Response {
+
+        private String userId;
+    }
+}
