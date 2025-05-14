4f466265eec0da09a6ea9c1169ac78bdb09d467b
jinhwanB <paewhal20@gmail.com>
2025-05-14 17:20:42 +0900

    feat: 아이디 찾기를 위한 DTO

---
+++ src/main/java/com/hwansol/moviego/member/dto/MemberFindIdDto.java
@@ -0,0 +1,32 @@
+package com.hwansol.moviego.member.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// 아이디 찾기를 위한 DTO
+public class MemberFindIdDto {
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @Builder(toBuilder = true)
+    public static class Request {
+
+        @NotBlank(message = "이메일을 입력해주세요.")
+        @Pattern(regexp = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", message = "올바른 이메일 형식을 입력해주세요.")
+        private String userEmail;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class Response {
+
+        private String userId;
+    }
+}
