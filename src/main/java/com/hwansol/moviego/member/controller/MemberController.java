7eeb1e90dc1301047eb84011ea93aa21c2d0c158
jinhwanB <paewhal20@gmail.com>
2025-05-18 17:55:04 +0900

    feat: 인증번호 이메일 발송 컨트롤러 구현

--- src/main/java/com/hwansol/moviego/member/controller/MemberController.java
+++ src/main/java/com/hwansol/moviego/member/controller/MemberController.java
@@ -7,12 +7,15 @@ import com.hwansol.moviego.member.model.Member;
 import com.hwansol.moviego.member.model.MemberDetails;
 import com.hwansol.moviego.member.service.MemberService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,7 +36,8 @@ public class MemberController {
      * @return 성공 시 200 상태코드와 성공 메시지, 실패 시 에러코드와 에러 메시지
      */
     @GetMapping("/member/{userId}")
-    public ResponseEntity<String> isDuplicatedId(@PathVariable String userId) {
+    public ResponseEntity<String> isDuplicatedId(
+        @NotBlank(message = "아이디를 입력해주세요.") @Pattern(regexp = "^[a-zA-Z][a-zA-Z0-9]*$", message = "아이디는 영문 + 숫자 조합으로 작성해야 합니다.") @PathVariable String userId) {
         memberService.duplicatedId(userId);
 
         return ResponseEntity.ok("사용가능한 아이디입니다.");
@@ -46,7 +50,8 @@ public class MemberController {
      * @return 성공 시 200 상태코드와 성공 메시지, 실패 시 에러코드와 에러 메시지
      */
     @GetMapping("/member/{userEmail}")
-    public ResponseEntity<String> isDuplicatedEmail(@PathVariable String userEmail) {
+    public ResponseEntity<String> isDuplicatedEmail(
+        @NotBlank(message = "아이디를 입력해주세요.") @Pattern(regexp = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", message = "올바른 이메일 형식을 입력해주세요.") @PathVariable String userEmail) {
         memberService.duplicatedEmail(userEmail);
 
         return ResponseEntity.ok("사용가능한 이메일입니다.");
@@ -76,8 +81,9 @@ public class MemberController {
      * @return 성공 시 200 코드와 성공 메시지, 실패 시 에러 코드와 에러 메시지
      */
     @GetMapping("/member")
-    public ResponseEntity<String> findPwController(@RequestParam String userId,
-        @RequestParam String userEmail) {
+    public ResponseEntity<String> findPwController(
+        @NotBlank(message = "아이디를 입력해주세요.") @Pattern(regexp = "^[a-zA-Z][a-zA-Z0-9]*$", message = "아이디는 영문 + 숫자 조합으로 작성해야 합니다.") @RequestParam String userId,
+        @NotBlank(message = "아이디를 입력해주세요.") @Pattern(regexp = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", message = "올바른 이메일 형식을 입력해주세요.") @RequestParam String userEmail) {
         memberService.findPw(userId, userEmail);
 
         return ResponseEntity.ok("이메일로 임시 비밀번호를 발급하였습니다. 로그인 이후 비밀번호 변경 바랍니다.");
@@ -98,5 +104,19 @@ public class MemberController {
         MemberGetDto.Response response = MemberGetDto.Response.from(member);
 
         return ResponseEntity.ok(response);
+    }
+
+    /**
+     * 인증번호 이메일 발송 컨트롤러
+     *
+     * @param userEmail 회원 이메일
+     * @return 성공 시 200 코드와 성공 메시지, 실패 시 에러코드와 에러메시지
+     */
+    @PostMapping("/auth")
+    public ResponseEntity<String> sendAuthNumController(
+        @NotBlank(message = "아이디를 입력해주세요.") @Pattern(regexp = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", message = "올바른 이메일 형식을 입력해주세요.") @RequestParam String userEmail) {
+        memberService.sendAuthNum(userEmail);
+
+        return ResponseEntity.ok("인증번호가 전송되었습니다.");
     }
 }
