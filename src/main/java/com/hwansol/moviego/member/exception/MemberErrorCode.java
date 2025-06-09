9ff0f39cc88e86b072e5ebf3bc23c884e7009818
jinhwanB <paewhal20@gmail.com>
2025-06-09 18:00:38 +0900

    update: 에러 상태 코드 변경

--- src/main/java/com/hwansol/moviego/member/exception/MemberErrorCode.java
+++ src/main/java/com/hwansol/moviego/member/exception/MemberErrorCode.java
@@ -12,11 +12,11 @@ public enum MemberErrorCode {
     NOT_COMPLETED_AUTH(HttpStatus.BAD_REQUEST.value(), "이메일 인증을 먼저 진행해주세요."),
     ORIGIN_EQUALS_NEW_OF_EMAIL(HttpStatus.BAD_REQUEST.value(), "변경할 이메일 주소가 기존 이메일 주소와 같습니다."),
     WRONG_PASSWORD(HttpStatus.BAD_REQUEST.value(), "올바르지 않는 비밀번호입니다."),
-    TIME_OVER_AUTH(HttpStatus.NO_CONTENT.value(), "인증번호 입력 시간이 초과되었습니다."),
+    TIME_OVER_AUTH(HttpStatus.BAD_REQUEST.value(), "인증번호 입력 시간이 초과되었습니다."),
     WRONG_AUTH_NUM(HttpStatus.BAD_REQUEST.value(), "올바른 인증번호가 아닙니다."),
     DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST.value(), "이미 사용중인 이메일입니다."),
     DUPLICATED_ID(HttpStatus.BAD_REQUEST.value(), "이미 사용중인 아이디입니다."),
-    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND.value(), "존재하지 않는 회원입니다.");
+    NOT_FOUND_MEMBER(HttpStatus.BAD_REQUEST.value(), "존재하지 않는 회원입니다.");
 
     private final int status;
     private final String message;
