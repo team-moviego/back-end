526353a45ebdad93d65582c9c71e5d719f8d77af
jinhwanB <paewhal20@gmail.com>
2025-06-13 18:23:39 +0900

    feat: 카카오 회원의 경우 에러코드 생성

--- src/main/java/com/hwansol/moviego/member/exception/MemberErrorCode.java
+++ src/main/java/com/hwansol/moviego/member/exception/MemberErrorCode.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum MemberErrorCode {
+    SOCIAL_USER(HttpStatus.BAD_REQUEST.value(), "카카오 소셜 회원 가입된 회원입니다."),
     WRONG_ORIGIN_PW(HttpStatus.BAD_REQUEST.value(), "현재 아이디의 비밀번호가 아닙니다."),
     DIFF_PW_AND_CONFIRM(HttpStatus.BAD_REQUEST.value(), "입력한 비밀번호와 비밀번호 확인이 일치하지 않습니다."),
     NOT_COMPLETED_AUTH(HttpStatus.BAD_REQUEST.value(), "이메일 인증을 먼저 진행해주세요."),
