0b5146b525ba582925ebc48282427322d1b828bd
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-08-10 23:02:04 +0900

    feat: cors 설정

--- src/main/java/com/hwansol/moviego/auth/SecurityConfig.java
+++ src/main/java/com/hwansol/moviego/auth/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.hwansol.moviego.auth;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hwansol.moviego.member.service.OAuth2UserService;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -11,12 +12,14 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -30,46 +33,61 @@ public class SecurityConfig {
     private final OAuth2UserService oAuth2UserService;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() { // security를 적용하지 않을 리소스
-        return web -> web.ignoring()
-            .requestMatchers("/error", "/favicon.ico");
-    }
-
+    
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable)
-            .formLogin(AbstractHttpConfigurer::disable)
-            .httpBasic(AbstractHttpConfigurer::disable)
-            .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(
-                SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(authorizeRequest -> authorizeRequest
-                .requestMatchers(HttpMethod.GET, "/api/members/member").authenticated()
-                .requestMatchers(HttpMethod.POST, "/api/members/member/signout").authenticated()
-                .requestMatchers(HttpMethod.PATCH, "/api/members/member/email").authenticated()
-                .requestMatchers(HttpMethod.PATCH, "/api/members/member/pw").authenticated()
-                .requestMatchers(HttpMethod.DELETE, "/api/members/member").authenticated()
-                .anyRequest().permitAll()
-            )
+                .cors(corsConfigure -> corsConfigure.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeRequest -> authorizeRequest
+                        .requestMatchers(HttpMethod.GET, "/api/members/member").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/api/members/member/signout").authenticated()
+                        .requestMatchers(HttpMethod.PATCH, "/api/members/member/email").authenticated()
+                        .requestMatchers(HttpMethod.PATCH, "/api/members/member/pw").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/api/members/member").authenticated()
+                        .anyRequest().permitAll()
+                )
 
-            // oauth2 설정
-            .oauth2Login(oauth ->
-                oauth.userInfoEndpoint(c -> c.userService(oAuth2UserService))
-                    .successHandler(oAuth2SuccessHandler)
-                    .failureHandler((request, response, exception) -> {
-                        log.error("OAuth2 로그인 실패", exception);
-                        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
-                            "OAuth2 로그인 실패: " + exception.getMessage());
-                    }))
+                // oauth2 설정
+                .oauth2Login(oauth ->
+                        oauth.userInfoEndpoint(c -> c.userService(oAuth2UserService))
+                                .successHandler(oAuth2SuccessHandler)
+                                .failureHandler((request, response, exception) -> {
+                                    log.error("OAuth2 로그인 실패", exception);
+                                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                                            "OAuth2 로그인 실패: " + exception.getMessage());
+                                }))
 
-            .exceptionHandling(exception -> exception.authenticationEntryPoint(
-                new JwtAuthenticationEntryPoint(objectMapper))
-            ) // 401, 403 에러 핸들러
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-            .headers(headersConfigurer -> headersConfigurer.frameOptions(
-                HeadersConfigurer.FrameOptionsConfig::disable));
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(
+                        new JwtAuthenticationEntryPoint(objectMapper))
+                ) // 401, 403 에러 핸들러
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .headers(headersConfigurer -> headersConfigurer.frameOptions(
+                        HeadersConfigurer.FrameOptionsConfig::disable));
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(
+                List.of("http://localhost:3000"));
+        corsConfiguration.setAllowedMethods(
+                List.of("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(
+                List.of("Authorization", "Content-Type", "New-AccessToken"));
+        corsConfiguration.setExposedHeaders(List.of("New-AccessToken"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setMaxAge(86400L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
     }
 }
