30f2d64bba5c6508a3cc5eb572ee465aec4218c1
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-08-17 16:45:30 +0900

    feat: 메인 또는 일반 포스터 구분용 enum 클래스

---
+++ src/main/java/com/hwansol/moviego/file/model/PosterType.java
@@ -0,0 +1,15 @@
+package com.hwansol.moviego.file.model;
+
+import com.hwansol.moviego.aop.EnumCreator;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@EnumCreator
+public enum PosterType {
+    MAIN("메인 포스터"),
+    NORMAL("일반 포스터");
+
+    private final String description;
+}
