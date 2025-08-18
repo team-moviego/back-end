d1de4abab3a1ed468d86666d176be0796932b4e7
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-08-18 13:17:14 +0900

    feat: 이미지 조회 시 dto 필드 추가

--- src/main/java/com/hwansol/moviego/image/dto/ImageGetDto.java
+++ src/main/java/com/hwansol/moviego/image/dto/ImageGetDto.java
@@ -2,6 +2,7 @@ package com.hwansol.moviego.image.dto;
 
 import com.hwansol.moviego.image.model.Image;
 import com.hwansol.moviego.image.model.PosterType;
+import java.io.Serializable;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,38 +13,41 @@ public class ImageGetDto {
     @Getter
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
     @Builder(toBuilder = true)
-    public static class Response {
+    public static class Response implements Serializable {
 
         private Long id;
-        private String url;
+        private String contentType;      // ex) "image/webp"
+        private byte[] imageData;        // 변환된 이미지 바이트 배열
         private PosterType posterType;
         private String imageName;
         private String extension;
         private long size;
 
-        public Response(Long id, String url, PosterType posterType, String imageName, String extension, long size) {
-            boolean isValidateDataFail = id == null || id <= 0 || url == null || url.isBlank() || posterType == null || imageName == null || imageName.isBlank() || extension == null || extension.isBlank() || size <= 0;
+        public Response(Long id, String contentType, byte[] imageData, PosterType posterType, String imageName, String extension, long size) {
+            boolean isValidateDataFail = id == null || id <= 0 || contentType == null || contentType.isBlank() || imageData == null || imageData.length == 0 || posterType == null || imageName == null || imageName.isBlank() || extension == null || extension.isBlank() || size <= 0;
 
             if (isValidateDataFail) {
                 throw new IllegalArgumentException("ImageGetDto.Response 생성 실패");
             }
 
             this.id = id;
-            this.url = url;
+            this.contentType = contentType;
+            this.imageData = imageData;
             this.posterType = posterType;
             this.imageName = imageName;
             this.extension = extension;
             this.size = size;
         }
 
-        public static ImageGetDto.Response from(Image image) {
+        public static ImageGetDto.Response from(Image image, String contentType, byte[] imageData) {
             return Response.builder()
                     .id(image.getId())
+                    .contentType(contentType)
+                    .imageData(imageData)
                     .extension(image.getExtension())
                     .imageName(image.getOriginImageName())
                     .posterType(image.getPosterType())
                     .size(image.getSize())
-                    .url(image.getUrl())
                     .build();
         }
     }
