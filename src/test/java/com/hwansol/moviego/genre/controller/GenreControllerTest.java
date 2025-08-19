9c9e6ac36ea4d892611fb650c7cb6a1c6ee93c0f
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-08-19 16:41:24 +0900

    test: 장르 컨트롤러 테스트

--- src/test/java/com/hwansol/moviego/genre/controller/GenreControllerTest.java
+++ src/test/java/com/hwansol/moviego/genre/controller/GenreControllerTest.java
@@ -1,7 +1,11 @@
 package com.hwansol.moviego.genre.controller;
 
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -11,6 +15,7 @@ import com.hwansol.moviego.auth.OAuth2SuccessHandler;
 import com.hwansol.moviego.auth.SecurityConfig;
 import com.hwansol.moviego.auth.TokenProvider;
 import com.hwansol.moviego.cookie.service.CookieService;
+import com.hwansol.moviego.genre.dto.GenreCreateDto;
 import com.hwansol.moviego.genre.model.Genre;
 import com.hwansol.moviego.genre.service.GenreService;
 import com.hwansol.moviego.member.model.Member;
@@ -23,6 +28,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -71,13 +77,107 @@ class GenreControllerTest {
 
     @Test
     @DisplayName("장르 생성 컨트롤러")
-    void createGenreController() {
-        Member.builder()
-                .role(Role)
-        new PrincipalDetails()
+    void createGenreController() throws Exception {
+        Member member = Member.builder()
+                .role(Role.ROLE_ADMIN)
+                .userEmail("test@naver.com")
+                .userId("test")
+                .userPw("1234")
+                .build();
+        PrincipalDetails principalDetails = new PrincipalDetails(member, null);
+        GenreCreateDto.Request request = GenreCreateDto.Request.builder()
+                .name("genre")
+                .build();
+        Genre genre = Genre.builder()
+                .name("genre")
+                .build();
+        genre.withId(1L);
+
+        when(genreService.createGenre(argThat(r -> r.getName().equals("genre")))).thenReturn(genre);
+
+        mockMvc.perform(post("/api/genres/genre")
+                        .with(user(principalDetails))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1));
     }
 
     @Test
-    void deleteGenreController() {
+    @DisplayName("장르 생성 컨트롤러 실패 - 비로그인 상태")
+    void createGenreControllerFail1() throws Exception {
+        GenreCreateDto.Request request = GenreCreateDto.Request.builder()
+                .name("genre")
+                .build();
+
+        mockMvc.perform(post("/api/genres/genre")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @DisplayName("장르 생성 컨트롤러 실패 - 권한 없음")
+    void createGenreControllerFail2() throws Exception {
+        Member member = Member.builder()
+                .role(Role.ROLE_USER)
+                .userEmail("test@naver.com")
+                .userId("test")
+                .userPw("1234")
+                .build();
+        PrincipalDetails principalDetails = new PrincipalDetails(member, null);
+        GenreCreateDto.Request request = GenreCreateDto.Request.builder()
+                .name("genre")
+                .build();
+
+        mockMvc.perform(post("/api/genres/genre")
+                        .with(user(principalDetails))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @DisplayName("장르 하드 삭제 컨트롤러")
+    void deleteGenreController() throws Exception {
+        Member member = Member.builder()
+                .role(Role.ROLE_ADMIN)
+                .userEmail("test@naver.com")
+                .userId("test")
+                .userPw("1234")
+                .build();
+        PrincipalDetails principalDetails = new PrincipalDetails(member, null);
+        Genre genre = Genre.builder()
+                .name("genre")
+                .build();
+        genre.withId(1L);
+
+        when(genreService.deleteGenre(1L)).thenReturn(genre);
+
+        mockMvc.perform(delete("/api/genres/genre/1")
+                        .with(user(principalDetails)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1));
+    }
+
+    @Test
+    @DisplayName("장르 하드 삭제 컨트롤러 실패 - pathVariable 유효성 검사 실패")
+    void deleteGenreControllerFail1() throws Exception {
+        Member member = Member.builder()
+                .role(Role.ROLE_ADMIN)
+                .userEmail("test@naver.com")
+                .userId("test")
+                .userPw("1234")
+                .build();
+        PrincipalDetails principalDetails = new PrincipalDetails(member, null);
+
+        mockMvc.perform(delete("/api/genres/genre/-1")
+                        .with(user(principalDetails)))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 }
