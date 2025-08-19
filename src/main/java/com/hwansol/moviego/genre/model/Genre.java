3798482ef4ddcfdf824ed9d1a6047ff1df712dfc
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-08-19 14:12:47 +0900

    feat: 장르 삭제 시 영화에 연결된 장르도 함께 삭제하기 위해 필드 추가

--- src/main/java/com/hwansol/moviego/genre/model/Genre.java
+++ src/main/java/com/hwansol/moviego/genre/model/Genre.java
@@ -1,12 +1,16 @@
 package com.hwansol.moviego.genre.model;
 
 import com.hwansol.moviego.config.BaseTImeEntity;
+import com.hwansol.moviego.movie.model.MovieGenre;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,16 +30,20 @@ public class Genre extends BaseTImeEntity {
     @Column(nullable = false)
     private String name;
 
+    @OneToMany(mappedBy = "genre", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<MovieGenre> movieGenres;
+
     @Column
     private LocalDateTime deletedAt;
 
     @Builder
-    public Genre(String name, LocalDateTime deletedAt) {
+    public Genre(String name, List<MovieGenre> movieGenres, LocalDateTime deletedAt) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Genre 엔티티 생성 실패");
         }
 
         this.name = name;
+        this.movieGenres = movieGenres;
         this.deletedAt = deletedAt;
     }
 }
