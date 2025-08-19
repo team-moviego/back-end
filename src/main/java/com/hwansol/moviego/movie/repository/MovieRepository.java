0ae6f92861d801030ef19265b16a3ecaef99e67a
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-08-19 12:37:24 +0900

    feat: 영화 레포지토리

---
+++ src/main/java/com/hwansol/moviego/movie/repository/MovieRepository.java
@@ -0,0 +1,10 @@
+package com.hwansol.moviego.movie.repository;
+
+import com.hwansol.moviego.movie.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long>, MovieCustomRepository {
+
+}
