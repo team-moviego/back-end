5e5bd62377381203b8a2382f10743f2365c9b577
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-08-19 15:27:00 +0900

    feat: 장르 하드 삭제 컨트롤러

--- src/main/java/com/hwansol/moviego/genre/controller/GenreController.java
+++ src/main/java/com/hwansol/moviego/genre/controller/GenreController.java
@@ -1,17 +1,21 @@
 package com.hwansol.moviego.genre.controller;
 
 import com.hwansol.moviego.genre.dto.GenreCreateDto;
+import com.hwansol.moviego.genre.dto.GenreDeleteDto;
 import com.hwansol.moviego.genre.dto.GenreGetDto;
 import com.hwansol.moviego.genre.model.Genre;
 import com.hwansol.moviego.genre.service.GenreService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,5 +60,21 @@ public class GenreController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(response);
+    }
+
+    /**
+     * 장르 하드 삭제 컨트롤러
+     *
+     * @param genreId 삭제할 장르 pk
+     * @return 성공 시 200 코드와 삭제한 장르 pk, 실패 시 에러코드와 에러메시지
+     */
+    @DeleteMapping("/genre/{genreId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<GenreDeleteDto.Response> deleteGenreController(@Positive(message = "pk는 0 또는 음수일 수 없습니다.") @PathVariable Long genreId) {
+        Genre genre = genreService.deleteGenre(genreId);
+
+        GenreDeleteDto.Response response = GenreDeleteDto.Response.from(genre);
+
+        return ResponseEntity.ok(response);
     }
 }
