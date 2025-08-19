dee6d1721e226992568591898ebd8708866e03de
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-08-19 15:20:13 +0900

    feat: 장르 생성 컨트롤러

--- src/main/java/com/hwansol/moviego/genre/controller/GenreController.java
+++ src/main/java/com/hwansol/moviego/genre/controller/GenreController.java
@@ -1,13 +1,19 @@
 package com.hwansol.moviego.genre.controller;
 
+import com.hwansol.moviego.genre.dto.GenreCreateDto;
 import com.hwansol.moviego.genre.dto.GenreGetDto;
 import com.hwansol.moviego.genre.model.Genre;
 import com.hwansol.moviego.genre.service.GenreService;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +39,22 @@ public class GenreController {
                 .toList();
 
         return ResponseEntity.ok(response);
+    }
+
+    /**
+     * 장르 생성 컨트롤러
+     *
+     * @param request GenreCreateDto.Request
+     * @return 성공 시 200 코드와 생성된 장르 pk, 실패 시 에러코드와 에러메시지
+     */
+    @PostMapping("/genre")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<GenreCreateDto.Response> createGenreController(@Valid @RequestBody GenreCreateDto.Request request) {
+        Genre genre = genreService.createGenre(request);
+
+        GenreCreateDto.Response response = GenreCreateDto.Response.from(genre);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(response);
     }
 }
