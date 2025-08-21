ff91caf7c59ceac5f0365ae224c8b4ff38c5b3e8
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-08-21 12:51:03 +0900

    feat: 전체 상영관 리스트 조회

--- src/main/java/com/hwansol/moviego/screen/service/ScreenService.java
+++ src/main/java/com/hwansol/moviego/screen/service/ScreenService.java
@@ -9,6 +9,8 @@ import com.hwansol.moviego.movieschedule.dto.MovieScheduleGetDto;
 import com.hwansol.moviego.screen.dto.ScreenGetDto;
 import com.hwansol.moviego.screen.model.Screen;
 import com.hwansol.moviego.screen.repository.ScreenRepository;
+import com.hwansol.moviego.seat.dto.SeatGetDto;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,28 +23,58 @@ public class ScreenService {
     private final ScreenRepository screenRepository;
     private final ImageService imageService;
 
+    /**
+     * 상영관 상세 조회
+     *
+     * @param screenId 조회할 상영관 pk
+     * @return 조회된 상영관의 응답 dto
+     */
     @Transactional(readOnly = true)
     public ScreenGetDto.Response getScreen(Long screenId) {
         Screen screen = screenRepository.findById(screenId)
                 .orElseThrow(NotFoundException::new);
 
-        List<MovieScheduleGetDto.Response> movieScheduleResponseList = screen.getMovieSchedules().stream()
-                .map(ms -> {
-                    Image image = ms.getMovie().getImages().stream()
-                            .filter(i -> i.getPosterType().equals(PosterType.MAIN))
-                            .findAny()
-                            .orElse(null);
+        return getScreenGetDtoResponse(screen);
+    }
 
-                    if (image == null) {
-                        return MovieScheduleGetDto.Response.from(ms, null);
-                    }
+    /**
+     * 전체 상영관 리스트 조회
+     *
+     * @return 조회된 전체 상영관의 응답 dto 리스트
+     */
+    @Transactional(readOnly = true)
+    public List<ScreenGetDto.Response> getScreenList() {
+        List<Screen> screenList = screenRepository.findAll();
 
-                    List<ImageGetDto.Response> imageResponse = imageService.getImageList(List.of(image));
+        return screenList.stream()
+                .map(this::getScreenGetDtoResponse)
+                .toList();
+    }
 
-                    return MovieScheduleGetDto.Response.from(ms, imageResponse.get(0));
-                })
+    // 상영관 조회 응답 dto 생성 메서드
+    private ScreenGetDto.Response getScreenGetDtoResponse(Screen screen) {
+        List<SeatGetDto.Response> seatList = screen.getSeats() == null ? new ArrayList<>() : screen.getSeats().stream()
+                .map(SeatGetDto.Response::from)
                 .toList();
 
-        return ScreenGetDto.Response.from(screen, movieScheduleResponseList);
+        List<MovieScheduleGetDto.Response> movieScheduleResponseList = screen.getMovieSchedules() == null ? new ArrayList<>() :
+                screen.getMovieSchedules().stream()
+                        .map(ms -> {
+                            Image image = ms.getMovie().getImages().stream()
+                                    .filter(i -> i.getPosterType().equals(PosterType.MAIN))
+                                    .findAny()
+                                    .orElse(null);
+
+                            if (image == null) {
+                                return MovieScheduleGetDto.Response.from(ms, null);
+                            }
+
+                            List<ImageGetDto.Response> imageResponse = imageService.getImageList(List.of(image));
+
+                            return MovieScheduleGetDto.Response.from(ms, imageResponse.get(0));
+                        })
+                        .toList();
+
+        return ScreenGetDto.Response.from(screen, seatList, movieScheduleResponseList);
     }
 }
