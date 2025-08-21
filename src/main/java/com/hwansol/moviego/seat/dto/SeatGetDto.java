363ff135096804ccc4942a727e69af090a29709d
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-08-21 10:59:55 +0900

    feat: 좌석 조회 응답 dto에 좌석열 필드 추가

--- src/main/java/com/hwansol/moviego/seat/dto/SeatGetDto.java
+++ src/main/java/com/hwansol/moviego/seat/dto/SeatGetDto.java
@@ -14,22 +14,25 @@ public class SeatGetDto {
     public static class Response {
 
         private Long id;
+        private String seatRow;
         private int seatNum;
 
-        public Response(Long id, int seatNum) {
-            boolean isValidateDataFail = id == null || id <= 0 || seatNum <= 0;
+        public Response(Long id, String seatRow, int seatNum) {
+            boolean isValidateDataFail = id == null || id <= 0 || seatRow == null || seatRow.isBlank() || seatNum <= 0;
 
             if (isValidateDataFail) {
                 throw new IllegalArgumentException("SeatGetDto.Response 생성 실패");
             }
 
             this.id = id;
+            this.seatRow = seatRow;
             this.seatNum = seatNum;
         }
 
         public static SeatGetDto.Response from(Seat seat) {
             return SeatGetDto.Response.builder()
                     .id(seat.getId())
+                    .seatRow(seat.getSeatRow())
                     .seatNum(seat.getSeatNum())
                     .build();
         }
