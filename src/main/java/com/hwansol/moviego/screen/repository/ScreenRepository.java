4dce6476dfed9bdc65500c29b480b542c3085cfa
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-08-22 19:54:16 +0900

    feat: 상영관 이름으로 엔티티 조회하는 메서드 추가

--- src/main/java/com/hwansol/moviego/screen/repository/ScreenRepository.java
+++ src/main/java/com/hwansol/moviego/screen/repository/ScreenRepository.java
@@ -1,9 +1,12 @@
 package com.hwansol.moviego.screen.repository;
 
 import com.hwansol.moviego.screen.model.Screen;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScreenRepository extends JpaRepository<Screen, Long> {
+
+    Optional<Screen> findByName(String name);
 }
