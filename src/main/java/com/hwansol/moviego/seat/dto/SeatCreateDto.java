0510d51999a9658c34cce94b28b1d79f7847430f
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-08-22 19:45:59 +0900

    feat: validation 추가

--- src/main/java/com/hwansol/moviego/seat/dto/SeatCreateDto.java
+++ src/main/java/com/hwansol/moviego/seat/dto/SeatCreateDto.java
@@ -1,6 +1,8 @@
 package com.hwansol.moviego.seat.dto;
 
 import com.hwansol.moviego.seat.model.Seat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,8 +15,13 @@ public class SeatCreateDto {
     @Builder(toBuilder = true)
     public static class Request {
 
+        @NotBlank(message = "좌석 열을 입력해주세요.")
         private String seatRow;
+
+        @Positive(message = "좌석번호는 0 또는 음수일 수 없습니다.")
         private int seatNum;
+
+        @Positive(message = "pk는 0 또는 음수일 수 없습니다.")
         private Long screenId;
 
         public Request(String seatRow, int seatNum, Long screenId) {
