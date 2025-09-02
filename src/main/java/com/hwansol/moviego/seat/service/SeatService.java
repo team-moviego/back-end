dd3b95a7a31d31e24ff4d1ec6fa7c56724ecda3f
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-09-02 18:00:52 +0900

    feat: 좌석 추가 생성 서비스

--- src/main/java/com/hwansol/moviego/seat/service/SeatService.java
+++ src/main/java/com/hwansol/moviego/seat/service/SeatService.java
@@ -1,6 +1,10 @@
 package com.hwansol.moviego.seat.service;
 
+import com.hwansol.moviego.common.DuplicatedException;
 import com.hwansol.moviego.common.NotFoundException;
+import com.hwansol.moviego.screen.model.Screen;
+import com.hwansol.moviego.screen.repository.ScreenRepository;
+import com.hwansol.moviego.seat.dto.SeatCreateDto;
 import com.hwansol.moviego.seat.model.Seat;
 import com.hwansol.moviego.seat.repository.SeatRepository;
 import java.util.List;
@@ -13,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class SeatService {
 
     private final SeatRepository seatRepository;
+    private final ScreenRepository screenRepository;
 
     /**
      * 좌석 상세 조회 서비스
@@ -34,5 +39,30 @@ public class SeatService {
     @Transactional(readOnly = true)
     public List<Seat> getSeatList() {
         return seatRepository.findAll();
+    }
+
+    /**
+     * 좌석 추가 생성 서비스
+     *
+     * @param request SeatCreateDto.Request
+     * @return 추가 생성된 좌석 엔티티
+     */
+    @Transactional
+    public Seat createSeat(SeatCreateDto.Request request) {
+        Screen screen = screenRepository.findById(request.getScreenId())
+                .orElseThrow(NotFoundException::new);
+
+        Seat seat = seatRepository.findBySeatRow(request.getSeatRow().toUpperCase())
+                .orElse(null);
+
+        boolean isDuplicated = seat != null && seat.getSeatNum() == request.getSeatNum() && seat.getScreen().getId().equals(screen.getId());
+        if (isDuplicated) {
+            throw new DuplicatedException();
+        }
+
+        Seat newSeat = request.toEntity();
+        newSeat.relatedScreen(screen);
+
+        return seatRepository.save(newSeat);
     }
 }
