114b1f26c99a3882805b992be87efdc3f52d0cc0
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-09-02 18:00:42 +0900

    feat: 좌석열을 통해 좌석 조회하는 메서드 추가

--- src/main/java/com/hwansol/moviego/seat/repository/SeatRepository.java
+++ src/main/java/com/hwansol/moviego/seat/repository/SeatRepository.java
@@ -1,10 +1,12 @@
 package com.hwansol.moviego.seat.repository;
 
 import com.hwansol.moviego.seat.model.Seat;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
 
+    Optional<Seat> findBySeatRow(String seatRow);
 }
