8a6c6f1ad611f900f48d3489563caee64b814a4a
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-09-03 14:28:12 +0900

    feat: 좌석 조회 시 상영관 정보 포함하도록 수정

--- src/main/java/com/hwansol/moviego/seat/dto/SeatGetDto.java
+++ src/main/java/com/hwansol/moviego/seat/dto/SeatGetDto.java
@@ -1,5 +1,6 @@
 package com.hwansol.moviego.seat.dto;
 
+import com.hwansol.moviego.screen.dto.ScreenSimpleGetDto;
 import com.hwansol.moviego.seat.model.Seat;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -16,9 +17,11 @@ public class SeatGetDto {
         private Long id;
         private String seatRow;
         private int seatNum;
+        private ScreenSimpleGetDto.Response screenInfo;
 
-        public Response(Long id, String seatRow, int seatNum) {
-            boolean isValidateDataFail = id == null || id <= 0 || seatRow == null || seatRow.isBlank() || seatNum <= 0;
+        public Response(Long id, String seatRow, int seatNum,
+                ScreenSimpleGetDto.Response screenInfo) {
+            boolean isValidateDataFail = id == null || id <= 0 || seatRow == null || seatRow.isBlank() || seatNum <= 0 || screenInfo == null;
 
             if (isValidateDataFail) {
                 throw new IllegalArgumentException("SeatGetDto.Response 생성 실패");
@@ -27,13 +30,18 @@ public class SeatGetDto {
             this.id = id;
             this.seatRow = seatRow;
             this.seatNum = seatNum;
+            this.screenInfo = screenInfo;
         }
 
         public static SeatGetDto.Response from(Seat seat) {
+            ScreenSimpleGetDto.Response screenInfo = ScreenSimpleGetDto.Response.from(
+                    seat.getScreen());
+
             return SeatGetDto.Response.builder()
                     .id(seat.getId())
                     .seatRow(seat.getSeatRow())
                     .seatNum(seat.getSeatNum())
+                    .screenInfo(screenInfo)
                     .build();
         }
     }
