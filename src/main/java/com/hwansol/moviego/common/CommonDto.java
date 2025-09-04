0b4bec33accbc473451b62dd010bbe81a56765d7
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-09-04 13:51:00 +0900

    feat: 공통으로 사용하는 dto 생성

--- src/main/java/com/hwansol/moviego/common/CommonDto.java
+++ src/main/java/com/hwansol/moviego/common/CommonDto.java
@@ -1,28 +1,49 @@
 package com.hwansol.moviego.common;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder(toBuilder = true)
 public class CommonDto {
 
-    private Long id;
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @Builder(toBuilder = true)
+    public static class DeleteRequest {
 
-    public CommonDto(Long id) {
-        if (id == null || id <= 0) {
-            throw new IllegalArgumentException("CommonDto.Response 생성 실패");
+        @NotBlank(message = "영구 삭제를 위한 문구를 입력해주세요.")
+        private String deleteString;
+
+        public DeleteRequest(String deleteString) {
+            if (deleteString == null || deleteString.isBlank()) {
+                throw new IllegalArgumentException("CommonDto.DeleteRequest 생성 실패");
+            }
+            
+            this.deleteString = deleteString;
         }
-
-        this.id = id;
     }
 
-    public static CommonDto from(Long id) {
-        return CommonDto.builder()
-                .id(id)
-                .build();
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @Builder(toBuilder = true)
+    public static class Response {
+
+        private Long id;
+
+        public Response(Long id) {
+            if (id == null || id <= 0) {
+                throw new IllegalArgumentException("CommonDto.Response 생성 실패");
+            }
+
+            this.id = id;
+        }
+
+        public static CommonDto.Response from(Long id) {
+            return CommonDto.Response.builder()
+                    .id(id)
+                    .build();
+        }
     }
 }
