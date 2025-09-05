b8225e75990396c16ddd46589e1d93d19e3fd643
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-09-05 16:41:57 +0900

    feat: 감독 생성 컨트롤러

--- src/main/java/com/hwansol/moviego/director/controller/DirectorController.java
+++ src/main/java/com/hwansol/moviego/director/controller/DirectorController.java
@@ -1,13 +1,19 @@
 package com.hwansol.moviego.director.controller;
 
+import com.hwansol.moviego.common.CommonDto;
+import com.hwansol.moviego.director.dto.DirectorCreateDto;
 import com.hwansol.moviego.director.dto.DirectorSimpleGetDto;
 import com.hwansol.moviego.director.service.DirectorService;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +36,21 @@ public class DirectorController {
         List<DirectorSimpleGetDto.Response> response = directorService.getDirectorList();
 
         return ResponseEntity.ok(response);
+    }
+
+    /**
+     * 감독 생성 컨틀롤러
+     *
+     * @param request 감독 생성에 필요한 이름 필드 정보를 가지고 있는 Request dto 클래스
+     * @return 성공 시 201 코드와 생성된 감독 엔티티의 pk 정보를 가지고 있는 dto, 실패 시 에러코드와 에러메시지
+     */
+    @PostMapping("/director")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<CommonDto.Response> createDirectorController(@Valid @RequestBody
+    DirectorCreateDto.Request request) {
+        CommonDto.Response response = directorService.createDirector(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(response);
     }
 }
