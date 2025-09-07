7bdd946e6196dc20e71c45e78f4a9d04bd440ef0
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-09-07 17:20:23 +0900

    feat: 감독명 변경 컨트롤러

--- src/main/java/com/hwansol/moviego/director/controller/DirectorController.java
+++ src/main/java/com/hwansol/moviego/director/controller/DirectorController.java
@@ -3,6 +3,7 @@ package com.hwansol.moviego.director.controller;
 import com.hwansol.moviego.common.CommonDto;
 import com.hwansol.moviego.director.dto.DirectorCreateDto;
 import com.hwansol.moviego.director.dto.DirectorSimpleGetDto;
+import com.hwansol.moviego.director.dto.DirectorUpdateNameDto;
 import com.hwansol.moviego.director.service.DirectorService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
@@ -14,6 +15,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -55,6 +57,21 @@ public class DirectorController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(response);
+    }
+
+    /**
+     * 감독명 변경 컨트롤러
+     *
+     * @param directorId 감독명을 변경할 감독 엔티티의 pk
+     * @param request    변경할 감독명의 정보를 가지고 있는 request dto
+     * @return 성공 시 200 코드와 감독명이 변경된 엔티티의 pk를 담고 있는 response dto, 실패 시 에러코드와 에러메시지
+     */
+    @PatchMapping("/director/{directorId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<CommonDto.Response> updateDirectorNameController(@Positive(message = "pk는 0 또는 음수일 수 없습니다.") @PathVariable Long directorId, @Valid @RequestBody DirectorUpdateNameDto.Request request) {
+        CommonDto.Response response = directorService.updateDirectorName(directorId, request);
+
+        return ResponseEntity.ok(response);
     }
 
     /**
