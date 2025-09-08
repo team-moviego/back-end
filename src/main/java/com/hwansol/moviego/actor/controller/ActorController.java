a301c8a43bf6df000ffdd4ffceb8a70c3df16720
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-09-08 13:14:25 +0900

    feat: 배우 생성 컨트롤러

--- src/main/java/com/hwansol/moviego/actor/controller/ActorController.java
+++ src/main/java/com/hwansol/moviego/actor/controller/ActorController.java
@@ -1,13 +1,19 @@
 package com.hwansol.moviego.actor.controller;
 
+import com.hwansol.moviego.actor.dto.ActorCreateDto;
 import com.hwansol.moviego.actor.dto.ActorSimpleGetDto;
 import com.hwansol.moviego.actor.service.ActorService;
+import com.hwansol.moviego.common.CommonDto;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +37,22 @@ public class ActorController {
         List<ActorSimpleGetDto.Response> response = actorService.getActorList();
 
         return ResponseEntity.ok(response);
+    }
+
+    /**
+     * 배우 생성 컨트롤러
+     * 관리자만 생성 가능
+     *
+     * @param request 배우 생성을 위한 배우명 정보를 담고 있는 request dto
+     * @return 성공 시 201 코드와 생성된 엔티티의 pk 정보를 담고 있는 response dto, 실패 시 에러코드와 에러메시지
+     */
+    @PostMapping("/actor")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<CommonDto.Response> createActorController(@Valid @RequestBody
+    ActorCreateDto.Request request) {
+        CommonDto.Response response = actorService.createActor(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(response);
     }
 }
