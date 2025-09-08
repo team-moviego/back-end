f6de28b9d306826cbbf99d340f4aa8860f78f790
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-09-08 13:17:39 +0900

    feat: 배우명 변경 컨트롤러

--- src/main/java/com/hwansol/moviego/actor/controller/ActorController.java
+++ src/main/java/com/hwansol/moviego/actor/controller/ActorController.java
@@ -2,9 +2,11 @@ package com.hwansol.moviego.actor.controller;
 
 import com.hwansol.moviego.actor.dto.ActorCreateDto;
 import com.hwansol.moviego.actor.dto.ActorSimpleGetDto;
+import com.hwansol.moviego.actor.dto.ActorUpdateNameDto;
 import com.hwansol.moviego.actor.service.ActorService;
 import com.hwansol.moviego.common.CommonDto;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,6 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -54,5 +58,24 @@ public class ActorController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(response);
+    }
+
+    /**
+     * 배우명 변경 컨트롤러
+     * 관리자만 변경 가능
+     *
+     * @param actorId 배우명을 변경할 엔티티 pk
+     * @param request 변경할 배우명 정보를 담고 있는 request dto
+     * @return 성공 시 200 코드와 배우명이 변경된 엔티티의 pk 정보를 담고 있는 response dto, 실패 시 에러코드와 에러메시지
+     */
+    @PatchMapping("/actor/{actorId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<CommonDto.Response> updateActorNameController(
+            @Positive(message = "pk는 0 또는 음수일 수 없습니다.") @PathVariable Long actorId,
+            @Valid @RequestBody
+            ActorUpdateNameDto.Request request) {
+        CommonDto.Response response = actorService.updateActorName(actorId, request);
+
+        return ResponseEntity.ok(response);
     }
 }
