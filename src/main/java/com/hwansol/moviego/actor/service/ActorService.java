b6ee3fa7a786f068570e7d588d124542eb62f24f
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-09-08 11:49:38 +0900

    feat: 배우 전체 리스트 조회 서비스

---
+++ src/main/java/com/hwansol/moviego/actor/service/ActorService.java
@@ -0,0 +1,30 @@
+package com.hwansol.moviego.actor.service;
+
+import com.hwansol.moviego.actor.dto.ActorSimpleGetDto;
+import com.hwansol.moviego.actor.model.Actor;
+import com.hwansol.moviego.actor.repository.ActorRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ActorService {
+
+    private final ActorRepository actorRepository;
+
+    /**
+     * 배우 전체 리스트 조회 서비스
+     *
+     * @return 조회된 전체 배우 리스트의 순수 정보를 담고 있는 dto 리스트
+     */
+    @Transactional(readOnly = true)
+    public List<ActorSimpleGetDto.Response> getActorList() {
+        List<Actor> actors = actorRepository.findAll();
+
+        return actors.stream()
+                .map(ActorSimpleGetDto.Response::from)
+                .toList();
+    }
+}
