2652d3e603b077495aa0b181d364aa0bdceef82e
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-09-08 11:17:10 +0900

    feat: 사용 안하는 코드 제거 및 핸들러 추가

--- src/main/java/com/hwansol/moviego/config/GlobalExceptionHandler.java
+++ src/main/java/com/hwansol/moviego/config/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.hwansol.moviego.config;
 
-import com.hwansol.moviego.auth.TokenException;
+import com.hwansol.moviego.common.BusinessLogicException;
+import com.hwansol.moviego.cookie.exception.CookieException;
 import com.hwansol.moviego.mail.exception.MailException;
 import com.hwansol.moviego.member.exception.MemberException;
+import com.hwansol.moviego.redis.exception.RedisException;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import java.util.ArrayList;
@@ -12,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -29,7 +32,8 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AuthorizationDeniedException.class)
-    private ResponseEntity<String> handleAuthorizationDeniedException(AuthorizationDeniedException e) {
+    private ResponseEntity<String> handleAuthorizationDeniedException(
+            AuthorizationDeniedException e) {
         log.error("403 Forbidden");
 
         return ResponseEntity.status(HttpStatus.FORBIDDEN)
@@ -42,25 +46,25 @@ public class GlobalExceptionHandler {
         log.error("404 NotFound", e);
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-            .body("존재하지 않는 endPoint입니다.");
+                .body("존재하지 않는 endPoint입니다.");
     }
 
     // 405 에러 핸들러
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     private ResponseEntity<String> handleNotSupportedException(
-        HttpRequestMethodNotSupportedException e) {
+            HttpRequestMethodNotSupportedException e) {
 
         log.error("405 NotSupported", e);
 
         return ResponseEntity
-            .status(HttpStatus.METHOD_NOT_ALLOWED)
-            .body("해당 url을 지원하지 않습니다. HTTP Method(GET, PUT, POST, DELETE)가 정확한지 확인해주세요.");
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body("해당 url을 지원하지 않습니다. HTTP Method(GET, PUT, POST, DELETE)가 정확한지 확인해주세요.");
     }
 
     // 유효성 검증 에러 핸들러(requestBody) -> 400 에러
     @ExceptionHandler(MethodArgumentNotValidException.class)
     private ResponseEntity<List<String>> handleValidException(
-        MethodArgumentNotValidException e) {
+            MethodArgumentNotValidException e) {
 
         log.error("request 유효성 검사 실패", e);
 
@@ -77,7 +81,7 @@ public class GlobalExceptionHandler {
     // 유효성 검증 에러 핸들러(pathVariable, requestParam) -> 400 에러
     @ExceptionHandler(ConstraintViolationException.class)
     private ResponseEntity<List<String>> handleValidException2(
-        ConstraintViolationException e) {
+            ConstraintViolationException e) {
 
         log.error("pathVariable 또는 requestParam 유효성 검사 실패", e);
 
@@ -93,55 +97,55 @@ public class GlobalExceptionHandler {
     // 필수 PathVariable 값 존재하지 않을 경우 에러 핸들러
     @ExceptionHandler(MissingPathVariableException.class)
     private ResponseEntity<String> handleMissingPathVariableException(
-        MissingPathVariableException e) {
+            MissingPathVariableException e) {
 
         log.error("필수 PathVariable 값 존재하지 않음", e);
 
         return ResponseEntity.badRequest().body(
-            "필수 값인 pathVariable 값이 존재하지 않습니다.");
+                "필수 값인 pathVariable 값이 존재하지 않습니다.");
     }
 
     // 필수 RequestPart 값 존재하지 않을 경우 에러 핸들러
     @ExceptionHandler(MissingServletRequestPartException.class)
     private ResponseEntity<String> handleMissingServletRequestPartException(
-        MissingServletRequestPartException e) {
+            MissingServletRequestPartException e) {
 
         log.error("필수 RequestPart 값 존재하지 않음", e);
 
         return ResponseEntity.badRequest()
-            .body("필수값인 RequestPart 값이 존재하지 않습니다.");
+                .body("필수값인 RequestPart 값이 존재하지 않습니다.");
     }
 
     // 필수 RequestParam 값 존재하지 않을 경우 에러 핸들러
     @ExceptionHandler(MissingServletRequestParameterException.class)
     private ResponseEntity<String> handleMissingServletRequestParameterException(
-        MissingServletRequestParameterException e) {
+            MissingServletRequestParameterException e) {
 
         log.error("필수 RequestParam 값 존재하지 않은", e);
 
         return ResponseEntity.badRequest()
-            .body("필수값인 RequestParam 값이 존재하지 않습니다.");
+                .body("필수값인 RequestParam 값이 존재하지 않습니다.");
     }
 
     // unique 제약 조건 위반 exception 핸들러
     @ExceptionHandler(DataIntegrityViolationException.class)
     private ResponseEntity<String> handleDataIntegrityViolationException(
-        DataIntegrityViolationException e) {
+            DataIntegrityViolationException e) {
 
         log.error("unique 제약 조건 위반", e);
 
         return ResponseEntity.badRequest()
-            .body("unique 제약 조건에 위반된 요청입니다. 생성 또는 변경하려는 요청 중 중복된 값이 포함되어있습니다.");
+                .body("unique 제약 조건에 위반된 요청입니다. 생성 또는 변경하려는 요청 중 중복된 값이 포함되어있습니다.");
     }
 
     @ExceptionHandler(HandlerMethodValidationException.class)
     private ResponseEntity<String> handleHandlerMethodValidationException(
-        HandlerMethodValidationException e) {
+            HandlerMethodValidationException e) {
 
         log.error("parameter 유효성 검증 실패", e);
 
         return ResponseEntity.badRequest()
-            .body("올바른 parameter 값이 아닙니다.");
+                .body("올바른 parameter 값이 아닙니다.");
     }
 
     @ExceptionHandler(MemberException.class)
@@ -149,7 +153,7 @@ public class GlobalExceptionHandler {
         log.error("회원 관련 에러 발생", e);
 
         return ResponseEntity.badRequest()
-            .body(e.getMessage());
+                .body(e.getMessage());
     }
 
     @ExceptionHandler(MailException.class)
@@ -157,20 +161,31 @@ public class GlobalExceptionHandler {
         log.error("메일 관련 에러 발생", e);
 
         return ResponseEntity.internalServerError()
-            .body(e.getMessage());
+                .body(e.getMessage());
     }
 
-    @ExceptionHandler(TokenException.class)
-    private ResponseEntity<String> handleTokenException(TokenException e) {
-        log.error("토큰 관련 에러 발생", e);
+    @ExceptionHandler(CookieException.class)
+    private ResponseEntity<String> handleCookieException(CookieException e) {
+        log.error("쿠키 관련 에러 발생", e);
 
-        if (e.getTokenErrorCode().getStatus() == 401) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+        return ResponseEntity.badRequest()
                 .body(e.getMessage());
-        }
+    }
 
-        return ResponseEntity.status(HttpStatus.FORBIDDEN)
-            .body(e.getMessage());
+    @ExceptionHandler(RedisException.class)
+    private ResponseEntity<String> handleRedisException(RedisException e) {
+        log.error("레디스 관련 에러 발생", e);
+
+        return ResponseEntity.badRequest()
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(BusinessLogicException.class)
+    private ResponseEntity<String> handleBusinessLoginException(BusinessLogicException e) {
+        log.error("비즈니스 로직 에러 발생", e);
+
+        return ResponseEntity.status(e.getErrorCode().getStatus())
+                .body(e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
@@ -178,6 +193,6 @@ public class GlobalExceptionHandler {
         log.error("예상하지 못한 에러 발생", e);
 
         return ResponseEntity.internalServerError()
-            .body("예상치 못한 에러가 발생했습니다. 서버에 문의해주세요.");
+                .body("예상치 못한 에러가 발생했습니다. 서버에 문의해주세요.");
     }
 }
