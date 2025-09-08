573685f12fe3f2fc9c4e96a0b16c8dc1fef06411
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-09-08 11:54:36 +0900

    feat: 배우 생성 서비스

--- src/main/java/com/hwansol/moviego/actor/service/ActorService.java
+++ src/main/java/com/hwansol/moviego/actor/service/ActorService.java
@@ -1,8 +1,11 @@
 package com.hwansol.moviego.actor.service;
 
+import com.hwansol.moviego.actor.dto.ActorCreateDto;
 import com.hwansol.moviego.actor.dto.ActorSimpleGetDto;
 import com.hwansol.moviego.actor.model.Actor;
 import com.hwansol.moviego.actor.repository.ActorRepository;
+import com.hwansol.moviego.common.CommonDto;
+import com.hwansol.moviego.common.DuplicatedException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -26,5 +29,26 @@ public class ActorService {
         return actors.stream()
                 .map(ActorSimpleGetDto.Response::from)
                 .toList();
+    }
+
+    /**
+     * 배우 생성 서비스
+     *
+     * @param request 생성할 배우의 배우명 정보를 가지고 있는 request dto
+     * @return 생성된 배우의 pk 정보를 담고 있는 response dto
+     */
+    @Transactional
+    public CommonDto.Response createActor(ActorCreateDto.Request request) {
+        Actor actor = actorRepository.findByName(request.getName())
+                .orElse(null);
+
+        if (actor != null) {
+            throw new DuplicatedException();
+        }
+
+        Actor newActor = request.toEntity();
+        Actor savedActor = actorRepository.save(newActor);
+
+        return CommonDto.Response.from(savedActor.getId());
     }
 }
