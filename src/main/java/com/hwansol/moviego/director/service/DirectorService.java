dafc5b6ad8048052982e1bb1c0db036b6a6f6215
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-09-08 11:16:26 +0900

    refacter: 감독 하드 삭제 서비스 주석 추가

--- src/main/java/com/hwansol/moviego/director/service/DirectorService.java
+++ src/main/java/com/hwansol/moviego/director/service/DirectorService.java
@@ -54,6 +54,13 @@ public class DirectorService {
         return CommonDto.Response.from(savedDirector.getId());
     }
 
+    /**
+     * 감독 하드 삭제 서비스
+     *
+     * @param directorId 삭제할 감독 pk
+     * @param request    영구 삭제를 위한 문구를 담고 있는 Request dto
+     * @return 삭제된 감독 엔티티의 pk 정보를 담고 있는 Response dto
+     */
     @Transactional
     public CommonDto.Response deleteDirector(Long directorId, CommonDto.DeleteRequest request) {
         Director director = directorRepository.findById(directorId)
