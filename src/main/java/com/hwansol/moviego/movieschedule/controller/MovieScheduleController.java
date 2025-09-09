9b48877175a22e7cb32acd717806e9d20d07dc7a
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-09-09 17:02:48 +0900

    feat: 영화 스케줄 상세 조회 컨트롤러

---
+++ src/main/java/com/hwansol/moviego/movieschedule/controller/MovieScheduleController.java
@@ -0,0 +1,36 @@
+package com.hwansol.moviego.movieschedule.controller;
+
+import com.hwansol.moviego.movieschedule.dto.MovieScheduleGetDto;
+import com.hwansol.moviego.movieschedule.service.MovieScheduleService;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/movie-schedules")
+@RequiredArgsConstructor
+@Validated
+public class MovieScheduleController {
+
+    private final MovieScheduleService movieScheduleService;
+
+    /**
+     * 영화 스케줄 상세 조회 컨트롤러
+     *
+     * @param movieScheduleId 조회할 영화 스케줄 pk
+     * @return 성공 시 200 코드와 조회된 영화 스케줄의 상세 정보를 담고 있는 response dto, 실패 시 에러코드와 에러메시지
+     */
+    @GetMapping("/movie-schedule")
+    public ResponseEntity<MovieScheduleGetDto.Response> getMovieSchedule(
+            @Positive(message = "pk는 0 또는 음수일 수 없습니다.") @RequestParam Long movieScheduleId
+    ) {
+        MovieScheduleGetDto.Response response = movieScheduleService.getMovieSchedule(movieScheduleId);
+
+        return ResponseEntity.ok(response);
+    }
+}
