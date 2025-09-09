5e8093261a344e0c6bba19c326a376b02379f4a3
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-09-09 13:55:12 +0900

    feat: 클래스명 변경

--- src/main/java/com/hwansol/moviego/screen/service/ScreenService.java
+++ src/main/java/com/hwansol/moviego/screen/service/ScreenService.java
@@ -7,7 +7,7 @@ import com.hwansol.moviego.common.exception.NotFoundException;
 import com.hwansol.moviego.image.dto.ImageGetDto;
 import com.hwansol.moviego.image.model.Image;
 import com.hwansol.moviego.image.service.ImageService;
-import com.hwansol.moviego.movieschedule.dto.MovieScheduleListGetDto;
+import com.hwansol.moviego.movieschedule.dto.MovieScheduleSimpleGetDto;
 import com.hwansol.moviego.screen.dto.ScreenCreateDto;
 import com.hwansol.moviego.screen.dto.ScreenGetDto;
 import com.hwansol.moviego.screen.model.Screen;
@@ -112,7 +112,7 @@ public class ScreenService {
                                         .thenComparing(SeatSimpleGetDto.Response::getSeatNum))
                         .toList();
 
-        List<MovieScheduleListGetDto.Response> movieScheduleResponseList =
+        List<MovieScheduleSimpleGetDto.Response> movieScheduleResponseList =
                 screen.getMovieSchedules() == null ? new ArrayList<>() :
                 screen.getMovieSchedules().stream()
                         .map(ms -> {
@@ -120,10 +120,10 @@ public class ScreenService {
                             List<ImageGetDto.Response> imageResponse = imageService.getImageList(
                                     imageList);
 
-                            return MovieScheduleListGetDto.Response.from(ms, imageResponse);
+                            return MovieScheduleSimpleGetDto.Response.from(ms);
                         })
                         .sorted(Comparator.comparing(
-                                MovieScheduleListGetDto.Response::getStartDateTime))
+                                MovieScheduleSimpleGetDto.Response::getStartDateTime))
                         .toList();
 
         return ScreenGetDto.Response.from(screen, seatList, movieScheduleResponseList);
