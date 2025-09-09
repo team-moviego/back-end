06334804e7a9e5357f5fc10f6754063aa232c9ac
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-09-09 15:04:16 +0900

    feat: 특정 상영관의 영화 스케줄 리스트 조회 서비스

--- src/main/java/com/hwansol/moviego/movieschedule/service/MovieScheduleService.java
+++ src/main/java/com/hwansol/moviego/movieschedule/service/MovieScheduleService.java
@@ -8,6 +8,8 @@ import com.hwansol.moviego.movieschedule.dto.MovieScheduleGetDto;
 import com.hwansol.moviego.movieschedule.dto.MovieScheduleSimpleGetDto;
 import com.hwansol.moviego.movieschedule.model.MovieSchedule;
 import com.hwansol.moviego.movieschedule.repository.MovieScheduleRepository;
+import com.hwansol.moviego.screen.model.Screen;
+import com.hwansol.moviego.screen.repository.ScreenRepository;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -20,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class MovieScheduleService {
 
     private final MovieScheduleRepository movieScheduleRepository;
+    private final ScreenRepository screenRepository;
     private final ImageService imageService;
 
     /**
@@ -46,7 +49,7 @@ public class MovieScheduleService {
     /**
      * 전체 영화 스케줄 리스트 조회 서비스
      *
-     * @return 조회된 전체 영화 스케줄의 간단 정보를 담고 있는 response dto 리스트
+     * @return 조회된 전체 영화 스케줄의 간단 정보를 담고 있는 response dto 리스트, 없을 경우 empty list
      */
     @Transactional(readOnly = true)
     public List<MovieScheduleSimpleGetDto.Response> getMovieScheduleList() {
@@ -63,5 +66,30 @@ public class MovieScheduleService {
         }
 
         return movieScheduleListResult;
+    }
+
+    /**
+     * 특정 상영관의 영화 스케줄 리스트 조회 서비스
+     *
+     * @param ScreenId 영화 스케줄을 조회할 상영관 pk
+     * @return 특정 상영관에서 조회된 영화 스케줄의 간단 정보를 담고 있는 response dto 리스트, 없을 경우 empty list
+     */
+    @Transactional(readOnly = true)
+    public List<MovieScheduleSimpleGetDto.Response> getMovieScheduleListByScreen(Long ScreenId) {
+        Screen screen = screenRepository.findById(ScreenId)
+                .orElseThrow(NotFoundException::new);
+
+        List<MovieSchedule> movieSchedules = screen.getMovieSchedules();
+        List<MovieScheduleSimpleGetDto.Response> movieScheduleList = new ArrayList<>();
+
+        if (movieSchedules != null && !movieSchedules.isEmpty()) {
+            movieScheduleList = movieSchedules.stream()
+                    .map(MovieScheduleSimpleGetDto.Response::from)
+                    .sorted(Comparator.comparing(
+                            MovieScheduleSimpleGetDto.Response::getStartDateTime))
+                    .toList();
+        }
+
+        return movieScheduleList;
     }
 }
