5c84dabf6afc13559e3a81ff7997373440104515
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-09-09 17:47:06 +0900

    refacter: 사용 안하는 어노테이션 제거

--- src/main/java/com/hwansol/moviego/movieschedule/model/MovieSchedule.java
+++ src/main/java/com/hwansol/moviego/movieschedule/model/MovieSchedule.java
@@ -20,12 +20,10 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@SQLRestriction("deleted_at IS NULL")
 public class MovieSchedule extends BaseTImeEntity {
 
     @Id
