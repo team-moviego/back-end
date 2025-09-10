8f00ae2fe3dd911dd14c2a1dcfac3aaf1e17ef3b
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-09-10 11:04:46 +0900

    feat: bulk 삭제를 위한 메서드 추가

--- src/main/java/com/hwansol/moviego/movieschedule/repository/MovieScheduleRepository.java
+++ src/main/java/com/hwansol/moviego/movieschedule/repository/MovieScheduleRepository.java
@@ -1,10 +1,18 @@
 package com.hwansol.moviego.movieschedule.repository;
 
 import com.hwansol.moviego.movieschedule.model.MovieSchedule;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieScheduleRepository extends JpaRepository<MovieSchedule, Long> {
 
+    @Modifying
+    @Query("delete " +
+           "from MovieSchedule ms " +
+           "where ms.id in :ids")
+    void bulkDeleteByIds(List<Long> ids);
 }
