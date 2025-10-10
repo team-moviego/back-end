715ca4c0f4cd6f9ed2631a6b15f67873e743f699
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-10-10 23:07:36 +0900

    feat: 예약 레포지토리

---
+++ src/main/java/com/hwansol/moviego/reservation/repository/ReservationRepository.java
@@ -0,0 +1,10 @@
+package com.hwansol.moviego.reservation.repository;
+
+import com.hwansol.moviego.reservation.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+}
