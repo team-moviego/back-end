2ddb95eb50ae01df374095fbc163a9d7af6f0807
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-10-11 17:03:15 +0900

    feat: 반환 타입 변경

--- src/main/java/com/hwansol/moviego/reservation/repository/ReservationRepository.java
+++ src/main/java/com/hwansol/moviego/reservation/repository/ReservationRepository.java
@@ -1,6 +1,7 @@
 package com.hwansol.moviego.reservation.repository;
 
 import com.hwansol.moviego.reservation.model.Reservation;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,5 +16,5 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
            "where r.member.userId = :memberId")
     Page<Reservation> findAllWithMemberId(String memberId, Pageable pageable);
 
-    Reservation findByReservationNum(String reservationNum);
+    Optional<Reservation> findByReservationNum(String reservationNum);
 }
