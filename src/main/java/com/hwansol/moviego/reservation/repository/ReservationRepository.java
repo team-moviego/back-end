9b5728b28e390f43e2559cc706601a7d07412fa4
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-10-11 16:33:26 +0900

    feat: 회원아이디로 예약 리스트 조회하는 레포지토리 메서드 추가

--- src/main/java/com/hwansol/moviego/reservation/repository/ReservationRepository.java
+++ src/main/java/com/hwansol/moviego/reservation/repository/ReservationRepository.java
@@ -1,10 +1,17 @@
 package com.hwansol.moviego.reservation.repository;
 
 import com.hwansol.moviego.reservation.model.Reservation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
+    @Query("select r " +
+           "from Reservation r " +
+           "where r.member.userId = :memberId")
+    Page<Reservation> findAllWithMemberId(String memberId, Pageable pageable);
 }
