aecadd55258a01cf68b4c6a62ce7955d3c92918b
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-10-19 17:50:08 +0900

    feat: 예약 생성 컨트롤러

--- src/main/java/com/hwansol/moviego/reservation/controller/ReservationController.java
+++ src/main/java/com/hwansol/moviego/reservation/controller/ReservationController.java
@@ -1,18 +1,26 @@
 package com.hwansol.moviego.reservation.controller;
 
+import com.hwansol.moviego.common.dto.CommonDto;
 import com.hwansol.moviego.common.dto.PageListResponseDto;
 import com.hwansol.moviego.member.model.PrincipalDetails;
+import com.hwansol.moviego.reservation.dto.ReservationCreateDto;
 import com.hwansol.moviego.reservation.dto.ReservationGetDto;
+import com.hwansol.moviego.reservation.model.Reservation;
 import com.hwansol.moviego.reservation.service.ReservationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,5 +63,23 @@ public class ReservationController {
         PageListResponseDto<ReservationGetDto.SimpleResponse> response = PageListResponseDto.from(reservationList);
 
         return ResponseEntity.ok(response);
+    }
+
+    /**
+     * 예약 생성 컨트롤러
+     *
+     * @param request          예약 생성에 필요한 정보 - 예약 타입, 가격, 좌석 정보
+     * @param movieScheduleId  예약하는 영화 스케줄 pk
+     * @param principalDetails 예약하는 회원의 principalDetails
+     * @return 성공 시 201 코드와 생성된 예약 pk, 실패 시 에러코드와 에러메시지
+     */
+    @PostMapping("/reservation/{movieScheduleId}")
+    public ResponseEntity<CommonDto.Response> createReservationController(@Valid @RequestBody ReservationCreateDto.Request request, @PathVariable Long movieScheduleId, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        String memberId = principalDetails.getUsername();
+        Reservation reservation = reservationService.createReservation(request, movieScheduleId, memberId);
+        CommonDto.Response response = CommonDto.Response.from(reservation.getId());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(response);
     }
 }
