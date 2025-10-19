577cfdfc6e3a0bf149325f75482a42085f6b1c4f
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2025-10-19 17:42:16 +0900

    feat: 회원의 예약 리스트 조회 컨트롤러

--- src/main/java/com/hwansol/moviego/reservation/controller/ReservationController.java
+++ src/main/java/com/hwansol/moviego/reservation/controller/ReservationController.java
@@ -1,9 +1,14 @@
 package com.hwansol.moviego.reservation.controller;
 
+import com.hwansol.moviego.common.dto.PageListResponseDto;
 import com.hwansol.moviego.member.model.PrincipalDetails;
 import com.hwansol.moviego.reservation.dto.ReservationGetDto;
 import com.hwansol.moviego.reservation.service.ReservationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
@@ -32,6 +37,22 @@ public class ReservationController {
     public ResponseEntity<ReservationGetDto.Response> getReservationController(@RequestParam Long reservationId, @AuthenticationPrincipal PrincipalDetails principalDetails) {
         String memberId = principalDetails.getUsername();
         ReservationGetDto.Response response = reservationService.getReservation(reservationId, memberId);
+
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * 회원의 예약 리스트 조회 컨트롤러
+     *
+     * @param principalDetails 조회하는 회원의 principalDetails
+     * @param pageable         페이지네이션 정보를 담은 pageable
+     * @return 성공 시 200 코드와 조회된 예약 리스트, 실패 시 에러코드와 에러메시지
+     */
+    @GetMapping
+    public ResponseEntity<PageListResponseDto<ReservationGetDto.SimpleResponse>> getReservationListController(@AuthenticationPrincipal PrincipalDetails principalDetails, @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+        String memberId = principalDetails.getUsername();
+        Page<ReservationGetDto.SimpleResponse> reservationList = reservationService.getReservationList(memberId, pageable);
+        PageListResponseDto<ReservationGetDto.SimpleResponse> response = PageListResponseDto.from(reservationList);
 
         return ResponseEntity.ok(response);
     }
